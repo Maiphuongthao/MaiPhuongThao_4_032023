03337be875c9cb4e40298226bab79d2ddd218141
Thao <thao.maiphuong@outlook.fr>
2023-03-12 21:12:08 +0100

    create class menu

---
+++ views/menu.py
@@ -0,0 +1,14 @@
+from ascii_chess import chess_tournaments_ascii
+
+
+class Menu:
+    def __init__(self) -> None:
+        pass
+
+    def title():
+        print(chess_tournaments_ascii)
+
+    def main_menu():
+        while True:
+            print(f"0: Add new player")
+            choice = input("Please add new player!")
